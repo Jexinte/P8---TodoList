c91e91d6a103d2dc450ffdac11b741c59d47d2f8
Yokke <mdembelepro@gmail.com>
2023-12-12 14:40:21 +0100

    refactor: unit test on UserTest

--- tests/Unit/Entity/UserTest.php
+++ tests/Unit/Entity/UserTest.php
@@ -3,9 +3,44 @@
 namespace App\Tests\Unit\Entity;
 
 use App\Entity\User;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use PHPUnit\Framework\TestCase;
 
-class UserTest extends KernelTestCase
+class UserTest extends TestCase
 {
+
+    public function getUser(): User
+    {
+        return new User();
+    }
+
+    public function testUsernameShouldReturnSameValue(): void
+    {
+        $this->getUser()->setUsername('Test');
+        $this->assertSame('Test', $this->getUser()->getUsername());
+    }
+
+    public function testPasswordShouldReturnSameValue(): void
+    {
+        $this->getUser()->setPassword('0000');
+        $this->assertSame('0000', $this->getUser()->getPassword());
+    }
+
+    public function testEmailShouldReturnSameValue(): void
+    {
+        $this->getUser()->setEmail('johndoe@gmail.com');
+        $this->assertSame('johndoe@gmail.com', $this->getUser()->getEmail());
+    }
+
+    public function testUserGroupShouldReturnSameValue(): void
+    {
+        $this->getUser()->setUserGroup("ROLE_USER");
+        $this->assertSame("ROLE_USER", $this->getUser()->getUserGroup());
+    }
+
+
+    public function testRolesShouldBeEmpty(): void
+    {
+        $this->assertEmpty($this->getUser()->getRoles());
+    }
 
 }
